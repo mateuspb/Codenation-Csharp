45d9adee5ad70397861ebca40cdd89a80c611a56
mateuspb <mateusperin@gmail.com>
2020-06-29 10:38:46 -0300

    Add files via upload

---
+++ semana06/csharp-5/Source/Services/QuoteService.cs
@@ -0,0 +1,35 @@
+using System.Collections.Generic;
+using System.Linq;
+using Codenation.Challenge.Models;
+
+namespace Codenation.Challenge.Services
+{
+    public class QuoteService : IQuoteService
+    {
+        private ScriptsContext _context;
+        private IRandomService _randomService;
+
+        public QuoteService(ScriptsContext context, IRandomService randomService)
+        {
+            this._context = context;
+            this._randomService = randomService;
+        }
+
+        public Quote GetAnyQuote()
+        {
+            var listOfQuotes = _context.Quotes.ToList();
+            return !listOfQuotes.Any()? null : GetNewQuote(listOfQuotes);
+        }
+
+        public Quote GetAnyQuote(string actor)
+        {
+            var listOfActorQuotes = _context.Quotes.Where(x => x.Actor == actor).ToList();
+            return !listOfActorQuotes.Any() ? null : GetNewQuote(listOfActorQuotes);
+        }
+
+        private Quote GetNewQuote(List<Quote> list)
+        {
+            return list[_randomService.RandomInteger(list.Count)];
+        }
+    }
+}
---
+++ semana06/csharp-5/Source/Services/RandomService.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Codenation.Challenge.Services
+{
+    public class RandomService: IRandomService
+    {
+        public int RandomInteger(int max)
+        {
+            var random = new Random();
+            return random.Next(max);
+        }
+    }
+}
